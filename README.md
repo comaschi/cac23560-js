b2919c6b2ea62377a52db5fba4af44f16ab99cf4
comaschi <comaschi@hotmail.com>
2023-11-03 17:54:12 -0300

    add JS changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cac23560-js
+
+Trabajo Practico JavaScript grupo 11 JAVAMOS comision 23560
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,27 @@
-const valueTicket = 200;
-const Category = {Estudiante:80, Trainee:50, Junior:15};
 
 
-console.log(Object.keys(Category));
+function calculo() {
 
-console.log(Category.Estudiante);
+    const ticket = 200;
+    let cantidad = document.getElementById('inputCity').value;
+    let subtotal = cantidad * ticket
+    let estudiante = 200 * cantidad - (subtotal * (80 / 100));
+    let trainee = 200 * cantidad - (subtotal * (50 / 100));
+    let junior = 200 * cantidad - (subtotal * (15 / 100));
+
+    if (document.getElementById("inputState").value == document.getElementById("estudiante").value) {
+        resultado.innerText = estudiante;
+    } else if (document.getElementById("inputState").value == document.getElementById("trainee").value) {
+        resultado.innerText = trainee;
+    } else if (document.getElementById("inputState").value == document.getElementById("junior").value) {
+        resultado.innerText = junior;
+    }
+}
+
+
+function reseteo() {
+    let resultado = document.getElementById("resultado");
+    resultado.innerText = 0;
+}
+
+reseteo();
